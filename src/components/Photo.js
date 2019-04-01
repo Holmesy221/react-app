72a2cf7a36c5ab3c82999c8c3706815828356de1
holmesy221 <holmesy221@gmail.com>
2019-04-01 07:22:18 +0100

    Updated stylesheet and more

---
+++ src/components/Photo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+
+class Photo extends Component {
+    render() {
+        const post = this.props.posts
+        return <figure className='figure'></figure>
+    }
+}
+
+export default Photo
